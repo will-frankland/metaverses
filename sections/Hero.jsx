d8e8b87a88163dadd77ff86c136d0eada843222c
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-03-08 11:18:28 -0500

    add styling to Hero section

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { motion } from "framer-motion";
 import styles from "../styles";
@@ -6,7 +6,23 @@ import { slideIn, staggerContainer, textVariant } from "../utils/motion";
 
 const Hero = () => (
   <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
-    Hero section
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <div className="flex justify-center items-center relative z-10">
+        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>Metaverse</motion.h1>
+        <motion.div
+          variants={textVariant(1.2)}
+          className="flex flex-row justify-center items-center"
+          >
+            <h1></h1>
+        </motion.div>
+      </div>
+    </motion.div>
   </section>
 );
 
