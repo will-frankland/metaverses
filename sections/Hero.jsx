08bd21d1cab5378466c2d30dc3fe2d6d13da0372
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-03-08 11:04:46 -0500

    add imports and initial styling

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,7 +1,11 @@
 'use client';
 
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { slideIn, staggerContainer, textVariant } from "../utils/motion";
+
 const Hero = () => (
-  <section>
+  <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
     Hero section
   </section>
 );
