835e9d72224985097d0d7585f55e22a6e657b71a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-03-08 10:37:43 -0500

    add imports to navbar component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,9 @@
 'use client';
 
+import { motion } from "framer-motion";
+import styles from '../styles';
+import { navVariants } from '../utils/motion';
+
 const Navbar = () => (
   <nav>
     navbar
