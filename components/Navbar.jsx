f0d82ba508d09e0d7d6666a12191f899793317d0
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-03-08 11:02:50 -0500

    add menu icon

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -21,6 +21,11 @@ const Navbar = () => (
       <h2 className="font-extrabold text=[24px] leading-[30px] text-white">
         METAVERSUS
       </h2>
+      <img
+        src="/menu.svg"
+        alt="image"
+        className="w-[24px] h-[24px] object-contain"
+        />
     </div>
   </motion.nav>
 );
