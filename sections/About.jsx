1cf459e572e0a54f5e948dde751152101d6f3920
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-03-13 09:22:37 -0400

    div change to self closing

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { motion } from "framer-motion";
 import { TypingText } from "../components";
@@ -7,21 +7,16 @@ import { fadeIn, staggerContainer } from "../utils/motion";
 
 const About = () => (
   <section className={`${styles.paddings} relative z-10`}>
-    <div className="gradient-02 z-0">
+    <div className="gradient-02 z-0" />
       <motion.div
         variants={staggerContainer}
         initial="hidden"
         whileInView="show"
-        viewport={{ once: false, amount: 0.25}}
+        viewport={{ once: false, amount: 0.25 }}
         className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
-        >
-          <TypingText
-          title="| About Metaversus"
-          textStyles="text-center"
-          />
+      >
+        <TypingText title="| About Metaversus" textStyles="text-center" />
       </motion.div>
-
-    </div>
     About section
   </section>
 );
