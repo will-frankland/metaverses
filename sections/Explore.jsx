4516bfa0cfea93c7ef29a64c41eb6d9d805aa0b5
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-03-13 09:40:46 -0400

    add imports to Explore page

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,5 +1,13 @@
 'use client';
 
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+import { ExploreCard, TitleText, TypingText } from "../components";
+import { staggerContainer } from "../utils/motion";
+import styles from "../styles";
+import { exploreWorlds } from '../constants';
+
 const Explore = () => (
   <section>
     Explore section
