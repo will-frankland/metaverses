7837504aabcf7db033cb9914b6299dbe4a920f9b
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-03-13 11:09:57 -0400

    add imports to GetStarted page

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,5 +1,12 @@
 'use client';
 
+import { motion } from "framer-motion";
+
+import { StartSteps, TitleText, TypingText } from "../components";
+import { staggerContainer, fadeIn, planetVariants } from "../utils/motion";
+import styles from "../styles";
+import { startingFeatures } from "../constants";
+
 const GetStarted = () => (
   <section>
     Get Started Section
