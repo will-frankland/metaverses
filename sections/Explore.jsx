441a8228b460f720376f8a3f5cd95891206de1b2
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-03-13 09:43:19 -0400

    add motion div with styling

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useState } from "react";
 import { motion } from "framer-motion";
@@ -6,11 +6,19 @@ import { motion } from "framer-motion";
 import { ExploreCard, TitleText, TypingText } from "../components";
 import { staggerContainer } from "../utils/motion";
 import styles from "../styles";
-import { exploreWorlds } from '../constants';
+import { exploreWorlds } from "../constants";
 
 const Explore = () => (
-  <section>
-    Explore section
+  <section className={`${styles.paddings}`} id="explore">
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      
+    </motion.div>
   </section>
 );
 
