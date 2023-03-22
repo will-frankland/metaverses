679f7f883a855de7876cb77d1499e20397aa7850
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-03-22 10:10:44 -0400

    add imports to Insights component

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,5 +1,12 @@
 'use client';
 
+import { motion } from "framer-motion";
+
+import { InsightCard, TitleText, TypingText } from "../components";
+import { staggerContainer } from "../utils/motion";
+import styles from "../styles";
+import { insights } from "../constants";
+
 const Insights = () => (
   <section>
     Insights section
