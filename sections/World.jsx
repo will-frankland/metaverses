b256bdf6eaac5155e5349461fadeb0f4fd5237d7
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-03-22 10:03:45 -0400

    add initial text and map to World section

--- sections/World.jsx
+++ sections/World.jsx
@@ -7,8 +7,35 @@ import { staggerContainer, fadeIn } from "../utils/motion";
 import styles from "../styles";
 
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{once: false, amount: 0.25}}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText
+        title="People on the World"
+        textStyles="text-center"
+      />
+      <TitleText 
+        title={<>
+        Track friends around you and invite them to play together in the same world
+        </>}
+        textStyles="text-center"
+      />
+      <motion.div
+        variant={fadeIn('up', 'tween', 0.3, 1)}
+        className="relative mt-[68px] flex w-full h-[550px]"
+      >
+        <img
+          src="/map.png"
+          alt="map"
+          className="w-full h-full object-cover"
+        />
+      </motion.div>
+    </motion.div>
   </section>
 );
 
