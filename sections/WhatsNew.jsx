b0d098435f1610b11991e53880848d6e798e15b7
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-03-22 09:37:51 -0400

    add imports to WhatsNew

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,5 +1,12 @@
 'use client';
 
+import { motion } from "framer-motion";
+
+import { NewFeatures, TitleText, TypingText } from "../components";
+import { staggerContainer, fadeIn, planetVariants } from "../utils/motion";
+import styles from "../styles";
+import { newFeatures } from "../constants";
+
 const WhatsNew = () => (
   <section>
     What's new section
