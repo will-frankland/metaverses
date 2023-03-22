750d3d20d225aaa63a106dd00371137aa8eda76c
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-03-22 09:56:51 -0400

    add imports to World file

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,5 +1,11 @@
 'use client';
 
+import { motion } from "framer-motion";
+
+import { TitleText, TypingText } from "../components";
+import { staggerContainer, fadeIn } from "../utils/motion";
+import styles from "../styles";
+
 const World = () => (
   <section>
     World section
