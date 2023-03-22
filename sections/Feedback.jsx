b7590cd414131eb35ec356bcbd1e802871e5806f
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-03-22 10:41:13 -0400

    add imports and copy initial div in Feedback component

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,8 +1,19 @@
 'use client';
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { fadeIn, staggerContainer, zoomIn } from "../utils/motion";
 
 const Feedback = () => (
-  <section>
-    Feedback section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{once: false, amount: 0.25}}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-6`}
+    >
+
+    </motion.div>
   </section>
 );
 
